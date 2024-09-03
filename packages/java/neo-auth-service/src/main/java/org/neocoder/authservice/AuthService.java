80483d890e5f8114b503bb0097b62a1c78247ef3
davidmendez9 <david.mf2004@gmail.com>
2024-09-03 19:24:57 +0200

    task(java) auth service scaffold #10

---
+++ packages/java/neo-auth-service/src/main/java/org/neocoder/authservice/AuthService.java
@@ -0,0 +1,34 @@
+package org.neocoder.authservice;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+/**
+ * AuthService is a class that provides methods for password management,
+ * including creating password hashes and comparing passwords.
+ * */
+public class AuthService {
+
+    private AuthService(){
+    }
+
+    /**
+     * Generates a secure hash for the given plain-text password.
+     *
+     * @param password The plain-text password to be hashed.
+     * @return A secure hash of the password.
+     */
+    public static String hashPassword(String password){
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    /**
+     * Compares a plain-text password with a previously hashed password.
+     *
+     * @param password The plain-text password to verify.
+     * @param hashedpassword The hashed password to compare.
+     * @return {@code true} if the password matches the hash, {@code false} otherwise.
+     */
+    public static boolean comparePassword(String password, String hashedpassword){
+        return BCrypt.checkpw(password, hashedpassword);
+    }
+}
---
+++ packages/java/neo-auth-service/src/main/java/org/neocoder/exception/InvalidTokenException.java
@@ -0,0 +1,11 @@
+package org.neocoder.exception;
+
+public class InvalidTokenException extends RuntimeException{
+    public InvalidTokenException(String message){
+        super(message);
+    }
+
+    public InvalidTokenException(String message, Throwable cause){
+        super(message, cause);
+    }
+}
