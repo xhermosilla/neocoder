f195aa3153001c28e70cdc0bff47a181779f939c
Antonio Hermosilla <hermosilla.antonio@gmail.com>
2024-09-09 10:50:34 +0200

    task(java auth service): add javadoc comments

--- packages/java/neo-auth-service/src/main/java/org/neocoder/services/auth/exception/InvalidTokenException.java
+++ packages/java/neo-auth-service/src/main/java/org/neocoder/services/auth/exception/InvalidTokenException.java
@@ -1,10 +1,23 @@
 package org.neocoder.services.auth.exception;
 
 public class InvalidTokenException extends RuntimeException {
+    /**
+     * Constructs a new {@code InvalidTokenException} with the specified detail
+     * message.
+     *
+     * @param message the detail message.
+     */
     public InvalidTokenException(String message) {
         super(message);
     }
 
+    /**
+     * Constructs a new {@code InvalidTokenException} with the specified detail
+     * message and cause.
+     *
+     * @param message the detail message.
+     * @param cause   the cause of the exception.
+     */
     public InvalidTokenException(String message, Throwable cause) {
         super(message, cause);
     }
