f4b2d242612ce8674f30abd121066c2e9d5dca8f
davidmendez9 <david.mf2004@gmail.com>
2024-09-16 16:51:46 +0200

    task(java): create auth microservice scaffold

---
+++ packages/java/neo-ms-auth-api/src/main/java/org/neocoder/neo_ms_auth_api/NeoMsAuthApiApplication.java
@@ -0,0 +1,13 @@
+package org.neocoder.neo_ms_auth_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoMsAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoMsAuthApiApplication.class, args);
+	}
+
+}
