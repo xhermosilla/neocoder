69d74441199cf635c990cc2b9ff14379371ee5fd
Antonio Hermosilla <hermosilla.antonio@gmail.com>
2024-09-24 07:09:59 +0200

    task: create base controllers #30

--- packages/java/neo-ms-auth-api/src/main/java/org/neocoder/microservices/auth/AuthController.java
+++ packages/java/neo-ms-auth-api/src/main/java/org/neocoder/microservices/auth/AuthController.java
@@ -1,96 +1,55 @@
 package org.neocoder.microservices.auth;
 
 import org.neocoder.microservices.auth.model.*;
-import org.neocoder.microservices.auth.neo_auth_service.Claims;
-import org.neocoder.microservices.auth.neo_auth_service.TokenService;
-import org.neocoder.microservices.auth.neo_auth_service.exception.InvalidTokenException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.Instant;
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
-import java.util.Collections;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final TokenService tokenService;
-    private static final String PASS = "admin";
+    private static final String DEFAULT_USER = "admin";
+    private static final String DEFAULT_PASSWORD = "admin";
 
-    public AuthController(TokenService tokenService) {
-        this.tokenService = tokenService;
-    }
-
-    // POST: /auth/login
+    /**
+     * Login endpoint.
+     *
+     * @param credentials LoginRequest object
+     * @return TokenResponse object
+     */
     @PostMapping("/login")
     public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest credentials) {
         String username = credentials.getUsername();
         String password = credentials.getPassword();
 
-        if (!PASS.equals(username) || !PASS.equals(password)) {
+        if (!DEFAULT_USER.equals(username) || !DEFAULT_PASSWORD.equals(password)) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
         }
 
-        // Generate the token
-        String token = tokenService.generate(username, Collections.singletonList(PASS));
-
-        // Decode the token to get the expiration time
-        Claims claims = tokenService.decode(token);
-
-        return ResponseEntity.ok(new TokenResponse(
-                claims.getExp(), // Expiration obtained from the generated token
-                token,
-                "Bearer"));
+        return ResponseEntity.ok(new TokenResponse(3800, "token", "Bearer"));
     }
 
-    // POST: /auth/refresh
+    /**
+     * Refresh endpoint.
+     *
+     * @param authHeader Authorization header
+     * @return TokenResponse object
+     */
     @PostMapping("/refresh")
     public ResponseEntity<TokenResponse> refresh(@RequestHeader("Authorization") String authHeader) {
-        String token = extractToken(authHeader);
-
-        try {
-            String refreshedToken = tokenService.refresh(token);
-
-            // Decode the new token to get the expiration time
-            Claims claims = tokenService.decode(refreshedToken);
-
-            return ResponseEntity.ok(new TokenResponse(
-                    claims.getExp(),
-                    refreshedToken,
-                    "Bearer"));
-        } catch (InvalidTokenException e) {
-            throw new InvalidTokenException("Unauthorized: Invalid token");
-        }
+        return ResponseEntity.ok(new TokenResponse(3800, "token", "Bearer"));
     }
 
-    // POST: /auth/validate
+    /**
+     * Validate endpoint.
+     *
+     * @param authHeader Authorization header
+     * @return ValidateTokenResponse object
+     */
     @PostMapping("/validate")
     public ResponseEntity<ValidateTokenResponse> validate(@RequestHeader("Authorization") String authHeader) {
-        String token = extractToken(authHeader);
-
-        try {
-            Claims claims = tokenService.verify(token);
-
-            // Format the expiration date
-            OffsetDateTime expiration = Instant.ofEpochSecond(claims.getExp()).atOffset(ZoneOffset.ofHours(2));
-
-            return ResponseEntity.ok(new ValidateTokenResponse(
-                    claims.getExp(),
-                    expiration.toString(),
-                    true));
-        } catch (InvalidTokenException e) {
-            throw new InvalidTokenException("Unauthorized: Invalid token");
-        }
+        return ResponseEntity.ok(new ValidateTokenResponse(3800, "3800", true));
     }
 
-    // Helper method to extract the token from the "Authorization" header.
-    private String extractToken(String authHeader) {
-        if (authHeader != null && authHeader.startsWith("Bearer ")) {
-            return authHeader.substring(7);
-        }
-        throw new InvalidTokenException("Invalid token");
-    }
 }
--- packages/java/neo-ms-auth-api/src/main/java/org/neocoder/microservices/auth/NeoAuthApplication.java
+++ packages/java/neo-ms-auth-api/src/main/java/org/neocoder/microservices/auth/NeoAuthApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NeoMsAuthApiApplication {
+public class NeoAuthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NeoMsAuthApiApplication.class, args);
+		SpringApplication.run(NeoAuthApplication.class, args);
 	}
 
 }
